d602dbb410a5e2842e6752da1393804cef04b672
bowen.zhou@okg.com <bowen.zhou@okg.com>
2023-01-17 16:36:40 +0800

    add nacos service

--- quickDocker.sh
+++ quickDocker.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 mvn clean compile package
 
 #clean docker container
@@ -13,6 +14,12 @@ docker build --build-arg nodeNumber=0 -t node:0 .
 docker build --build-arg nodeNumber=1 -t node:1 .
 docker build --build-arg nodeNumber=2 -t node:2 .
 
-docker run -d --shm-size=1G -p 8080:8080 --name node0 node:0
-docker run -d --shm-size=1G -p 8081:8080 --name node1 node:1
-docker run -d --shm-size=1G -p 8082:8080 --name node2 node:2
+#create a docker network
+docker network create --subnet=127.127.0.0/16 staticnet
+
+# run nacos server
+docker run -d -p 8848:8848 --restart always -e MODE=standalone --name nacos --net staticnet --ip 172.18.0.12 centralx/nacos-server:2.0.4
+
+docker run -d --shm-size=1G -p 8080:8080 --name  node0 --net staticnet --ip 172.18.0.20 node:0
+docker run -d --shm-size=1G -p 8081:8080 --name  node1 --net staticnet --ip 172.18.0.21 node:1
+docker run -d --shm-size=1G -p 8082:8080 --name  node2 --net staticnet --ip 172.18.0.22 node:2
